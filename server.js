4377a4454daaf32d019035669920d27d0453fb0e
Felix Schwamb <felix.schwamb@gmail.com>
2019-12-01 21:46:45 +0100

    Using Node File System to store bookmarks, adding nodemon.json

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
+const fs = require("fs");
 const express = require("express");
 
 const getDataByName = require("./middleware/repoData");
-const bookmarkData = require("./bookmarkData");
+// const bookmarkData = require("./bookmarkData");
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
+
+// /bookmarkData.json has been added to /nodemon.json, so it gets ignored by Nodemon and a change in the file does not trigger a restart
+fs.writeFileSync("./bookmarkData.json", "[]");
 
 // // Endpoints
 // // Get all reservations
@@ -20,6 +24,7 @@ app.get("/api/repositories/:name", getDataByName, async (req, res) => {
 
 app.get("/api/bookmarks", async (req, res) => {
 	try {
+		const bookmarkData = JSON.parse(fs.readFileSync("./bookmarkData.json"));
 		res.status(200).send(bookmarkData);
 	} catch (e) {
 		res.status(500).send();
@@ -29,7 +34,9 @@ app.get("/api/bookmarks", async (req, res) => {
 app.post("/api/bookmarks", async (req, res) => {
 	try {
 		const data = req.body;
+		const bookmarkData = JSON.parse(fs.readFileSync("./bookmarkData.json"));
 		bookmarkData.push(data);
+		fs.writeFileSync("./bookmarkData.json", JSON.stringify(bookmarkData));
 
 		res.status(200).send();
 	} catch (e) {
@@ -40,10 +47,13 @@ app.post("/api/bookmarks", async (req, res) => {
 app.delete("/api/bookmarks/:id", async (req, res) => {
 	try {
 		const id = Number(req.params.id);
+		const bookmarkData = JSON.parse(fs.readFileSync("./bookmarkData.json"));
 		const index = bookmarkData.findIndex(item => {
 			return item.id === id;
 		});
 		bookmarkData.splice(index, 1);
+
+		fs.writeFileSync("./bookmarkData.json", JSON.stringify(bookmarkData));
 
 		res.status(200).send();
 	} catch (e) {
